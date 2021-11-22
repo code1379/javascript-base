26dfab6149b3cc839d622e86dbe67de629e09ea7
Guo <2benumber1@sina.cn>
2021-11-22 17:37:36 +0800

    存存管理和GC垃圾回收机制

--- 03_js的内存管理/02_引用计数的代码.js
+++ 03_js的内存管理/02_引用计数的代码.js
@@ -4,3 +4,10 @@ var p = { name: "james", friends: obj }; // 引用了 obj
 
 // retain 0x00001  count = 3
 // info.friends = null // retain obj count = 2
+
+// 这些引用都指向 0x000001，那么我的计数器就加 1
+// 当计数器为 0 时，就会被自动回收
+
+// 弊端：循环引用
+var obj1 = { friend: obj2 };
+var obj2 = { friend: obj1 };
